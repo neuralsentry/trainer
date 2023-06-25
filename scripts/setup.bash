f5a2d788ddeeb0df2983c1286252d7eff750e024
Edison Chan <edisonchanwk@gmail.com>
2023-06-26 07:45:31 +0800

    chore: add sample scripts

---
+++ scripts/setup.bash
@@ -0,0 +1,3 @@
+pip install -r requirements.txt
+huggingface-cli login --token $1
+wandb login $2
---
+++ scripts/train-distilbert-imdb-mlm.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+export WANDB_PROJECT="hf-distilbert-imdb-mlm"
+
+
+python ./training/hf_trainer.py \
+--model_path 'distilbert-base-uncased' \
+--tokenizer_path 'distilbert-base-uncased' \
+--dataset_path './data/imdb-tokenized-mlm' \
+--output_dir "./models/$WANDB_PROJECT" \
+--overwrite_output_dir true \
+--hub_model_id "$WANDB_PROJECT" \
+--push_to_hub true \
+--task_name 'mlm' \
+--report_to 'none' \
+--do_train \
+--do_eval \
+--num_train_epochs 20 \
+--max_seq_length 128 \
+--per_device_train_batch_size 64 \
+--per_device_eval_batch_size 64 \
+--seed=420 \
+--data_seed=420 \
+--optim 'adamw_torch_fused' \
+--learning_rate 2e-5 \
+--lr_scheduler_type 'linear' \
+--weight_decay 1e-2 \
+--mlm_probability 0.15 \
+--fp16 true \
+--train_split 0.5 \
+--use_lora true \
+--evaluation_strategy 'epoch' \
+--logging_strategy 'epoch' \
+--save_strategy 'epoch' \
+--hub_strategy 'every_save' \
+--save_total_limit 2
