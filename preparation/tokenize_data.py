eb02c9951022dafcaa2f8763b9416511e7f5359e
Edison Chan <edisonchanwk@gmail.com>
2023-06-26 01:06:19 +0800

    feat: update tokenize_data.py
    - Use hf datasets for loading and saving
    - Add args for setting special tokens
    - Change input from jsonl to csv and hf
    - --max-length now required

--- preparation/tokenize_data.py
+++ preparation/tokenize_data.py
@@ -3,37 +3,38 @@ import argparse
 import logging
 import multiprocessing
 
-import pandas as pd
-import pyarrow as pa
-import numpy as np
-from parallel_pandas import ParallelPandas
+import datasets
+from datasets import load_dataset
 from transformers import AddedToken, AutoTokenizer
-from transformers.tokenization_utils import PreTrainedTokenizer
 
 LOG = logging.getLogger(__name__)
 logging.basicConfig(
-    format='[%(asctime)s] [%(levelname)s] %(message)s',
+    format="[%(asctime)s] [%(levelname)s] %(message)s",
     level=logging.DEBUG,
 )
 
-IGNORE_INDEX = -100
-
-# When appending EOS to the generations, append this many times. Seems helpful
-# when training on long (e.g.: many paragraphs/300+ words) examples.
-NUM_OF_EOS_TOKENS = 3
 
 def main() -> None:
     args = _parse_args_from_argv()
-    cpu_count = multiprocessing.cpu_count()
-    LOG.info("Preparing to use %s CPU cores...", cpu_count)
-    ParallelPandas.initialize(
-        n_cpu=cpu_count,
-        split_factor=4,
-        disable_pr_bar=False,
-    )
 
-    LOG.info("Loading tokenizer...")
+    logging.info(f"Loading tokenizer from {args.tokenizer_path}...")
     tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_path)
+
+    if args.mask_token is not None:
+        LOG.info(f"Set mask token to {args.mask_token}")
+        tokenizer.mask_token = args.mask_token
+
+    if args.pad_token is not None:
+        LOG.info(f"Set pad token to {args.pad_token}")
+        tokenizer.pad_token = args.pad_token
+
+    if args.sep_token is not None:
+        LOG.info(f"Set sep token to {args.sep_token}")
+        tokenizer.sep_token = args.sep_token
+
+    if args.cls_token is not None:
+        LOG.info(f"Set cls token to {args.cls_token}")
+        tokenizer.cls_token = args.cls_token
 
     if args.add_special_tokens is not None:
         # MAINTENANCE(11b): Big fat warning: the snippet below is copy-pasted
@@ -47,53 +48,39 @@ def main() -> None:
                 # exclusive to the Rust-based tokenizers? If anything seems
                 # funky about the special token behavior, this is a good place
                 # to look.
-                content, lstrip=True, rstrip=True)
+                content,
+                lstrip=True,
+                rstrip=True,
+            )
             for content in special_token_contents
         ]
 
-        tokenizer.add_special_tokens(
-            {"additional_special_tokens": special_tokens})
+        tokenizer.add_special_tokens({"additional_special_tokens": special_tokens})
 
-    # Load the entire dataset into memory. Hopefully we won't be working with
-    # huge files anytime soon! If this becomes a problem we can use Dask.
-    LOG.info("Loading entire dataset into memory...")
-    df = pd.read_json(args.input_file, lines=True)
-    LOG.info("Done! About to tokenize...")
+    LOG.info(f"Loading dataset from {args.input_file}...")
 
-    # Length warning messes up progress bars, so we silence temporarily.
-    # https://github.com/huggingface/transformers/issues/991
-    logging.getLogger("transformers.tokenization_utils_base").setLevel(
-        logging.ERROR)
+    raw_dataset: datasets.Dataset
+    if args.input_file.endswith(".csv"):
+        raw_dataset = load_dataset("csv", data_files=args.input_file, split="train")
+    else:
+        raw_dataset = load_dataset(args.input_file, split="train")
 
-    # `executor=threads` drastically slows down the tokenization, but
-    # it's a necessary evil. parallel_pandas seems to leak file descriptors when
-    # used in multiprocessing mode, and pandarallel deadlocks.
-    df = df.p_apply(
-        lambda x: _process_training_example(tokenizer, x),
-        axis=1,
-        executor="threads",
+    LOG.info(f"Tokenizing and trimming dataset...")
+
+    tokenized_dataset = raw_dataset.map(
+        lambda sample: tokenizer(
+            sample[args.column_name], truncation=True, max_length=args.max_length
+        ),
+        batched=True,
     )
 
-    logging.getLogger("transformers.tokenization_utils_base").setLevel(
-        logging.WARNING)
+    if "label" in tokenized_dataset.column_names:
+        LOG.info("Renaming `label` column to `labels`...")
+        tokenized_dataset = tokenized_dataset.rename_column("label", "labels")
 
-    # Trim out anything bigger than our max length to avoid problems at training
-    # time.
-    LOG.info("Done! Trimming out any examples longer than %s tokens...",
-             args.max_length)
+    LOG.info(f"Done! Saving to {args.output_file}...")
 
-    df = df.loc[df["input_ids"].map(len) <= args.max_length]
-
-    LOG.info("Done! Converting into an Apache Arrow table...")
-
-    # Convert the DataFrame of the training set into an Apache Arrow table and
-    # write out as a file that can be mmapped at training time.
-    table = pa.Table.from_pandas(df)
-
-    LOG.info("Writing out tokenized dataset...")
-    with pa.OSFile(args.output_file, 'wb') as sink:
-        with pa.RecordBatchFileWriter(sink, table.schema) as writer:
-            writer.write_table(table)
+    tokenized_dataset.save_to_disk(args.output_file)
 
     LOG.info("Finished.")
 
@@ -104,7 +91,7 @@ def _parse_args_from_argv() -> argparse.Namespace:
         "-i",
         "--input-file",
         required=True,
-        help="Path to the input JSONL file.",
+        help="Path to the input CSV file or HuggingFace dataset.",
     )
     parser.add_argument(
         "-o",
@@ -122,80 +109,56 @@ def _parse_args_from_argv() -> argparse.Namespace:
         "-l",
         "--max-length",
         type=int,
-        default=2048,
-        help=
-        "Max length in tokens before a training example is discarded. Defaults to 2048.",
+        required=True,
+        help="Max length in tokens before a training example is discarded. Defaults to 2048.",
     )
     parser.add_argument(
         "-s",
         "--add-special-tokens",
         type=str,
         default=None,
-        help="Extra special tokens to add to the tokenizer before tokenizing. Comma-separated."
+        help="Extra special tokens to add to the tokenizer before tokenizing. Comma-separated.",
+    )
+    parser.add_argument(
+        "-w",
+        "--n-workers",
+        type=int,
+        default=multiprocessing.cpu_count() // 2,
+        help="Number of workers to use for CPU-bound tasks. Defaults to half the number of CPU cores.",
+    )
+    parser.add_argument(
+        "-c",
+        "--column-name",
+        type=str,
+        default="text",
+        help="Name of the column in the input CSV file that contains the text to tokenize. Defaults to 'text'.",
+    )
+    parser.add_argument(
+        "--mask-token",
+        type=str,
+        default=None,
+        help="Token to use for masking. Defaults to the tokenizer's default mask token.",
+    )
+    parser.add_argument(
+        "--pad-token",
+        type=str,
+        default=None,
+        help="Token to use for padding. Defaults to the tokenizer's default pad token.",
+    )
+    parser.add_argument(
+        "--sep-token",
+        type=str,
+        default=None,
+        help="Token to use for separating sentences. Defaults to the tokenizer's default sep token.",
+    )
+    parser.add_argument(
+        "--cls-token",
+        type=str,
+        default=None,
+        help="Token to use for separating sentences. Defaults to the tokenizer's default cls token.",
     )
 
     return parser.parse_args()
-
-
-def _process_training_example(
-    tokenizer: PreTrainedTokenizer,
-    series: pd.Series,
-    append_eos: bool = True,
-) -> pd.Series:
-    # This is a single row so we _theoretically_ don't have to do this, but if
-    # we don't we get a scary warning.
-    # https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#returning-a-view-versus-a-copy
-    generation = series.loc["generation"]
-
-    # TODO(11b): Do a more robust check here.
-    is_llama = tokenizer.eos_token == "</s>"
-
-    if append_eos:
-        for _ in range(NUM_OF_EOS_TOKENS):
-            # As it turns out, with LLaMA's tokenizer, if you just append EOS to
-            # the end of the text it gets tokenized as a "</s>" text literal,
-            # and not as token #2 which is the _actual_ EOS token. You must have
-            # a space before "</s>" so it becomes token #2 as expected.
-            #
-            # I found this out after wasting 60+ GPU hours training a broken
-            # model :)
-            if is_llama:
-                generation += f" {tokenizer.eos_token}"
-            else:
-                generation += tokenizer.eos_token
-
-    prompt_tokens = tokenizer(series["prompt"],
-                              return_tensors="np").input_ids[0]
-
-    # The LLaMA tokenizer will add a BOS token whenever you tokenize
-    # something by default. If we allow this to happen in the response segment,
-    # it will cause wildly inconsistent behaviors where the model itself will
-    # learn to output BOS in the middle of sentences depending on how input
-    # tokenization is done. Not great, so we just force-disable BOS on the
-    # response segment.
-    response_tokenizer_kwargs = {"add_special_tokens": False} if is_llama else {}
-    response_tokens = tokenizer(generation,
-                                return_tensors="np",
-                                **response_tokenizer_kwargs).input_ids[0]
-
-    input_ids = np.concatenate([prompt_tokens, response_tokens], axis=-1)
-
-    # Let's not waste any more GPU time thanks to this.
-    if append_eos:
-        assert input_ids[-1].item() == tokenizer.eos_token_id, \
-            "EOS was not correctly appended to the end of the response tokens."
-
-    prompt_length = prompt_tokens.shape[-1]
-    labels = np.concatenate([
-        np.full((prompt_length), IGNORE_INDEX),
-        response_tokens,
-    ],
-                            axis=-1)
-
-    return pd.Series({
-        "input_ids": input_ids,
-        "labels": labels,
-    })
 
 
 if __name__ == "__main__":
