31a7dde59e95ff3e2f0342b93af676121336863f
Edison Chan <edisonchanwk@gmail.com>
2023-06-28 00:38:00 +0800

    chore: update bash training scripts

--- scripts/train-distilbert-git-commits-mlm.bash
+++ scripts/train-distilbert-git-commits-mlm.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-model_name="starencoder-git-commits-mlm"
+model_name="distilbert-git-commits-mlm"
 export WANDB_PROJECT="git-commits-mlm"
 report_to="wandb"
 # report_to="none"
@@ -18,13 +18,13 @@ config_training_output_dir="models/$model_name"
 config_training_overwrite_output_dir=true
 config_training_do_train=true
 config_training_do_eval=true
-config_training_per_device_train_batch_size=96
-config_training_per_device_eval_batch_size=96
+config_training_per_device_train_batch_size=128
+config_training_per_device_eval_batch_size=128
 config_training_fp16=true
 config_training_learning_rate=1e-4
 config_training_seed=420
 config_training_data_seed=420
-config_training_num_train_epochs=30
+config_training_num_train_epochs=20
 config_training_optim="adamw_hf"
 config_training_weight_decay=1e-2
 config_training_lr_scheduler_type="linear"
@@ -39,13 +39,9 @@ config_training_report_to=$report_to
 config_training_hub_strategy=$hub_strategy_EVERY_SAVE
 config_training_push_to_hub=true
 config_training_hub_model_id="neuralsentry/$model_name"
-config_model_model_name_or_path="bigcode/starencoder"
+config_model_model_name_or_path="distilbert-base-uncased"
 config_model_model_revision="main"
 config_model_cache_dir=".cache"
-config_model_mask_token="<mask>"
-config_model_pad_token="<pad>"
-config_model_sep_token="<sep>"
-config_model_cls_token="<cls>"
 config_data_dataset_name="neuralsentry/git-commits"
 config_data_max_seq_length=256
 config_data_validation_split_percentage=20
@@ -89,11 +85,7 @@ python training/run_mlm.py \
   --validation_split_percentage $config_data_validation_split_percentage \
   --preprocessing_num_workers $config_data_preprocessing_num_workers \
   --mlm_probability $config_data_mlm_probability \
-  --text_column_name $config_data_text_column_name \
-  --mask_token $config_model_mask_token \
-  --pad_token $config_model_pad_token \
-  --sep_token $config_model_sep_token \
-  --cls_token $config_model_cls_token # \
+  --text_column_name $config_data_text_column_name # \
   # --max_train_samples $config_data_max_train_samples \
   # --max_eval_samples $config_data_max_eval_samples \
 
