86287ed9b2a94ba728ddf976b81539355e6f17c3
Edison Chan <edisonchanwk@gmail.com>
2023-07-03 02:13:47 +0800

    refactor: move args to separate module

--- training/run_mlm.py
+++ training/run_mlm.py
@@ -9,9 +9,8 @@ from dataclasses import dataclass, field
 
 import datasets
 import evaluate
-from datasets import load_dataset
-
 import transformers
+from datasets import load_dataset
 from transformers.trainer_utils import get_last_checkpoint
 from transformers import (
     AutoModelForMaskedLM,
@@ -24,170 +23,21 @@ from transformers import (
     set_seed,
 )
 
-
-@dataclass
-class ModelArguments:
-    """
-    Arguments pertaining to which model/config/tokenizer we are going to fine-tune, or train from scratch.
-    """
-
-    model_name_or_path: str = field(
-        metadata={"help": ("The model checkpoint for weights initialization.")},
-    )
-    tokenizer_name_or_path: Optional[str] = field(
-        default=None,
-        metadata={
-            "help": "Pretrained tokenizer name or path if not the same as model_name"
-        },
-    )
-    cache_dir: Optional[str] = field(
-        default=None,
-        metadata={
-            "help": "Where do you want to store the pretrained models downloaded from huggingface.co"
-        },
-    )
-    use_fast_tokenizer: bool = field(
-        default=True,
-        metadata={
-            "help": "Whether to use one of the fast tokenizer (backed by the tokenizers library) or not."
-        },
-    )
-    model_revision: str = field(
-        default="main",
-        metadata={
-            "help": "The specific model version to use (can be a branch name, tag name or commit id)."
-        },
-    )
-    mask_token: Optional[str] = field(
-        metadata={
-            "help": "Token to use for masking. Defaults to the tokenizer's default mask token."
-        },
-        default=None,
-    )
-    pad_token: Optional[str] = field(
-        metadata={
-            "help": "Token to use for padding. Defaults to the tokenizer's default pad token."
-        },
-        default=None,
-    )
-    sep_token: Optional[str] = field(
-        metadata={
-            "help": "Token to use for separating sentences. Defaults to the tokenizer's default sep token."
-        },
-        default=None,
-    )
-    cls_token: Optional[str] = field(
-        metadata={
-            "help": "Token to use for separating sentences. Defaults to the tokenizer's default cls token."
-        },
-        default=None,
-    )
+from args import ModelArguments, DataTrainingArguments
 
 
 @dataclass
-class DataTrainingArguments:
-    """
-    Arguments pertaining to what data we are going to input our model for training and eval.
-    """
-
-    max_seq_length: int = field(
-        metadata={
-            "help": (
-                "The maximum total input sequence length after tokenization. Sequences longer "
-                "than this will be truncated."
-            )
-        },
-    )
-    dataset_name: Optional[str] = field(
-        default=None,
-        metadata={"help": "The name of the dataset to use (via the datasets library)."},
-    )
-    dataset_config_name: Optional[str] = field(
-        default=None,
-        metadata={
-            "help": "The configuration name of the dataset to use (via the datasets library)."
-        },
-    )
-    train_file: Optional[str] = field(
-        default=None, metadata={"help": "The input training data file (a text file)."}
-    )
-    validation_file: Optional[str] = field(
-        default=None,
-        metadata={
-            "help": "An optional input evaluation data file to evaluate the perplexity on (a text file)."
-        },
-    )
-    overwrite_cache: bool = field(
-        default=False,
-        metadata={"help": "Overwrite the cached training and evaluation sets"},
-    )
-    validation_split_percentage: Optional[int] = field(
-        default=5,
-        metadata={
-            "help": "The percentage of the train set used as validation set in case there's no validation split"
-        },
-    )
-    preprocessing_num_workers: Optional[int] = field(
-        default=multiprocessing.cpu_count() // 2,
-        metadata={"help": "The number of processes to use for the preprocessing."},
-    )
+class MLMDataTrainingArguments(DataTrainingArguments):
     mlm_probability: float = field(
         default=0.15,
         metadata={"help": "Ratio of tokens to mask for masked language modeling loss"},
     )
-    max_train_samples: Optional[int] = field(
-        default=None,
-        metadata={
-            "help": (
-                "For debugging purposes or quicker training, truncate the number of training examples to this "
-                "value if set."
-            )
-        },
-    )
-    max_eval_samples: Optional[int] = field(
-        default=None,
-        metadata={
-            "help": (
-                "For debugging purposes or quicker training, truncate the number of evaluation examples to this "
-                "value if set."
-            )
-        },
-    )
-    text_column_name: Optional[str] = field(
-        default="text",
-        metadata={
-            "help": "The name of the column in the datasets containing the full texts (used for language modeling)."
-        },
-    )
-
-    def __post_init__(self):
-        if (
-            self.dataset_name is None
-            and self.train_file is None
-            and self.validation_file is None
-        ):
-            raise ValueError(
-                "Need either a dataset name or a training/validation file."
-            )
-        else:
-            if self.train_file is not None:
-                extension = self.train_file.split(".")[-1]
-                if extension not in ["csv", "json", "txt"]:
-                    raise ValueError(
-                        "`train_file` should be a csv, a json or a txt file."
-                    )
-            if self.validation_file is not None:
-                extension = self.validation_file.split(".")[-1]
-                if extension not in ["csv", "json", "txt"]:
-                    raise ValueError(
-                        "`validation_file` should be a csv, a json or a txt file."
-                    )
 
 
 def main():
     # Parse Arguments
     parser = HfArgumentParser(
-        (ModelArguments, DataTrainingArguments, TrainingArguments)
+        (ModelArguments, MLMDataTrainingArguments, TrainingArguments)
     )
     model_args, data_args, training_args = parser.parse_args_into_dataclasses()
 
